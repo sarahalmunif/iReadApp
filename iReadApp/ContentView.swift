7c897ea5b193aae8252fee01067948faac56553b
saramuneef <saralmunif@gmail.com>
2020-08-03 01:36:44 +0300

    Initial Commit

---
+++ iReadApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iReadApp
+//
+//  Created by Sara Muneef on 13/12/1441 AH.
+//  Copyright © 1441 Sara Muneef. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iReadAppTests/iReadAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  iReadAppTests.swift
+//  iReadAppTests
+//
+//  Created by Sara Muneef on 13/12/1441 AH.
+//  Copyright © 1441 Sara Muneef. All rights reserved.
+//
+
+import XCTest
+@testable import iReadApp
+
+class iReadAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
